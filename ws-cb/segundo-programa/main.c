6f2201016a4c7b014bc038231b3ec5af5cac78fe
eilincoln <lincoln.santospro@gmail.com>
2023-10-08 17:04:36 -0300

    feito o segundo programa

--- ws-cb/segundo-programa/main.c
+++ ws-cb/segundo-programa/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    int idade;
+    double salario, altura;
+    char genero;
+    char nome[50];
+
+    idade = 20;
+    salario = 5800.5;
+    altura = 1.63;
+    genero = 'F';
+    strcpy(nome, "kamilly amorim");
+
+    printf("IDADE = %d\n", idade);
+    printf("SALARIO = %.2lf\n", salario);
+    printf("ALTURA = %.2lf\n", altura);
+    printf("GENERO = %c\n", genero);
+    printf("NOME = %s\n", nome);
+
+
     printf("Hello world!\n");
     return 0;
 }
