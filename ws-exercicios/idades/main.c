ba4b3ab6bd2228200b8195f4b9c1d6cf3b1ba6fa
eilincoln <lincoln.santospro@gmail.com>
2023-10-10 11:09:11 -0300

    feito pull e adicionando mais exercicios

---
+++ ws-exercicios/idades/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ler_texto(char *buffer, int length) {
+    fgets(buffer, length, stdin);
+    strtok(buffer, "\n");
+}
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main()
+{
+    char nome1[50], nome2[50];
+    int idade1, idade2;
+    double media;
+
+    printf("Dados da primeira pessoa: \n");
+    printf("Nome: ");
+    limpar_entrada();
+    ler_texto(nome1, 50);
+    printf("Idade: ");
+    scanf("%d", &idade1);
+
+    printf("Dados da segunda pessoa: \n");
+    printf("Nome: ");
+    limpar_entrada();
+    ler_texto(nome2, 50);
+    printf("Idade: ");
+    scanf("%d", &idade2);
+
+    media = (idade1 + idade2) / 2;
+
+    printf("A idade media de %s e %s é de %.1lf anos\n", nome1, nome2, media);
+
+    printf("Hello world!\n");
+    return 0;
+}
